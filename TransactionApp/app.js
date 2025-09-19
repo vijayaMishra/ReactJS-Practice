b6338a00b022fa4845960e6ed1966f6a76a7d32d
vijayaMishra <vijayamishra1709@gmail.com>
2025-09-19 17:44:03 +0530

    created basic sketch of Transaction App

---
+++ TransactionApp/app.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div class="header">Transaction App</div>
+    )
+}
+
+const rootElement = document.querySelector("#root");
+ReactDOM.render(<App />, rootElement);
