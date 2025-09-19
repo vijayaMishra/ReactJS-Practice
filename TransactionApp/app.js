2993adec605f106395a7e084656dae815f718247
vijayaMishra <vijayamishra1709@gmail.com>
2025-09-19 19:54:28 +0530

    managed to get a single common "Transaction App" header for entire application

--- TransactionApp/app.js
+++ TransactionApp/app.js
@@ -1,8 +1,28 @@
-function App() {
+function TransactionHeader() {
     return (
         <div class="header">Transaction App</div>
     )
 }
 
+function App() {
+    return (
+        <div>
+            <div>Date: </div>
+            <input></input>
+            <div>Amount: </div>
+            <input></input>
+            <div>Details: </div>
+            <input></input>
+        </div>
+    )
+}
+
 const rootElement = document.querySelector("#root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <div>
+        <TransactionHeader />
+        <App />
+        <App />
+    </div>,
+    rootElement
+);
