cb38f260b5bcc14715e703832463a49bd3e58cd5
vijayaMishra <vijayamishra1709@gmail.com>
2025-09-23 17:49:54 +0530

    wrote onChangeHandler function for each of the field and creating an array transctions for all the transaction data

--- TransactionApp/app.js
+++ TransactionApp/app.js
@@ -12,32 +12,54 @@ function App() {
 
     const [transactions, setTransactions] = React.useState([]);
     function addTransactionDetails() {
-
+        const newTransaction = { date, day, amount, details };
+        setTransactions([...transactions, newTransaction]);
+        //clearing userInput after the new transaction is added
+        setDate("");
+        setDay("");
+        setAmount(0);
+        setDetails("");
     }
 
+    function dateChangeHandler(event) {
+        
+        setDate(event.target.value);
+    }
+
+    function dayChangeHandler(event) {
+        console.log("day : event.target.value", event.target.value);
+        setDay(event.target.value);
+    }
+
+    function amountChangeHandler(event) {
+        setAmount(event.target.value);
+    }
+    function detailsChangeHandler(event) {
+        setDetails(event.target.value);
+    }
     return (
         <div>
             <div className="displayBeside">
                 <div>Date: </div>
-                <input type="date"/>
+                <input type="date" onChange={dateChangeHandler}/>
             </div>
 
             <div className="displayBeside">
                 <div>Day: </div>
-                <input className="inputLine" type="text" placeholder="Day" />
+                <input className="inputLine" type="text" placeholder="Day" onChange={dayChangeHandler} />
             </div>
 
             <div className="displayBeside">
                 <div>Amount: </div>
-                <input className="inputLine" type="number" placeholder="Amount in Rs" />
+                <input className="inputLine" type="number" placeholder="Amount in Rs" onChange={amountChangeHandler}/>
             </div>
 
             <div className="displayBeside">
                 <div>Details: </div>
-                <input className="inputLine" />
+                <input className="inputLine" onClick={detailsChangeHandler}/>
             </div>
             <div className="addDetailsBtnParent">
-                <button className="addDetailsBtn" onClick={addTransactionDetails()}>Add details</button>
+                <button className="addDetailsBtn" onClick={addTransactionDetails}>Add details</button>
             </div>
             <div className="lineSeparation"></div>
         </div>
