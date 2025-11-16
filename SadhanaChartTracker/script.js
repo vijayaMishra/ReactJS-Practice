319df4e12c67c0bd182ddb7bc51744710861bf87
vijayatiwari <vijayamishra1709@gmail.com>
2025-11-16 19:30:50 +0530

    created table structure for sadhana card

--- SadhanaChartTracker/script.js
+++ SadhanaChartTracker/script.js
@@ -4,21 +4,41 @@ function SadhnaCard() {
         <h2 className="main-heading">Sadhana Card</h2>
         <h5>Q. What is that one thing which pleases Maharaj?</h5>
         <h5>A. Following the authorities, focused in Sadhana and Hearing</h5>
-        <div className="one-liner">
-            <h3 className="day">Day</h3>
-            <h3 className="date" type="date">Date</h3>
-            <h3 className="day">Got-Up Time</h3>
-            <h3 className="day">Japa-Time</h3>
-            {/* <input type="time"></input> */}
-        </div>
-        <div className="one-liner">
-            <h3 className="dash-line"></h3>
-            <h3 className="dash-line"></h3>
-            <h3 className="dash-line"></h3>
-            <h3 className="dash-line"></h3>
-        </div>
+        
+        <table>
+            <thead>    
+                <tr>
+                    <th>Day</th>
+                    <th>Date</th>
+                    <th>Got-up @ </th>
+                    <th>Jap start-time</th>
+                    <th>Jap end-time</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        
+        </table>
     </div>
-};
+}
 
 // Render the component to the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
