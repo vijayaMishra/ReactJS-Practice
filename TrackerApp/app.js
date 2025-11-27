3c74b6168973b2330f417e5f0a457aba549a2a80
vijayatiwari <vijayamishra1709@gmail.com>
2025-11-27 14:10:01 +0530

    trying to get the accordions workkk

--- TrackerApp/app.js
+++ TrackerApp/app.js
@@ -1,5 +1,19 @@
 //get all accordions
+const accordions = document.querySelectorAll(".accordion");
 
+// add event listener in for each accordion
+for(let i=0; i<accordions.length; i++) {
+    accordions[i].addEventListener("click", function toggleAccordion() {
+        this.classList.toggle("active");
 
-// add event listener in for each
+        const sectionDetails = this.nextElementSibling;
+
+        if(sectionDetails.style.display === "none") {
+            sectionDetails.style.display = "block";
+        } else {
+            sectionDetails.style.display = "none"
+        }
+
+    })
+}
 
