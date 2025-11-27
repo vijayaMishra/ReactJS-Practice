0a86528c1f1d4ace3bf2d1f16a2b520e55147617
vijayatiwari <vijayamishra1709@gmail.com>
2025-11-27 15:44:18 +0530

    when display is set as none is .css file, JavaScript takes sectionDetails.style.display as '' instead of none, so fixing the issue to get expand/collapse feature work on the first click as well

--- TrackerApp/app.js
+++ TrackerApp/app.js
@@ -6,9 +6,9 @@ for(let i=0; i<accordions.length; i++) {
     accordions[i].addEventListener("click", function toggleAccordion() {
         this.classList.toggle("active");
 
-        const sectionDetails = this.nextElementSibling;
+        const sectionDetails = this.nextElementSibling; //it is getting div details that has class "section-details"
 
-        if(sectionDetails.style.display === "none") {
+        if(sectionDetails.style.display === "none" || sectionDetails.style.display === "") {
             sectionDetails.style.display = "block";
         } else {
             sectionDetails.style.display = "none"
